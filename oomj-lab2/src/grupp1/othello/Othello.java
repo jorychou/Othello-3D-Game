eb71fcd4b78938f60cf3b33a299728acc569a2e7
Philip Arvidsson <philiparvidsson@users.noreply.github.com>
2015-10-08 18:02:55 +0200

    Style.

--- oomj-lab2/src/grupp1/othello/Othello.java
+++ oomj-lab2/src/grupp1/othello/Othello.java
@@ -1,9 +1,17 @@
 package grupp1.othello;
 
+/*------------------------------------------------
+ * IMPORTS
+ *----------------------------------------------*/
+
 import javafx.application.Application;
 import javafx.scene.control.Button;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+/*------------------------------------------------
+ * CLASS
+ *----------------------------------------------*/
 
 /**
  * The program's main class. This class contains the entry-point.
@@ -11,6 +19,20 @@ import javafx.stage.Stage;
  * @author Philip Arvidsson (S133686)
  */
 public final class Othello extends Application {
+
+/*------------------------------------------------
+ * PUBLIC METHODS
+ *----------------------------------------------*/
+
+/**
+ * Program entry point.
+ * @param args Command line arguments.
+ */
+public static void main(String[] args) {
+    printIntroMessage();
+
+    launch(args);
+}
 
 /**
  * This method initializes the game and starts it.
@@ -27,14 +49,17 @@ public void start(Stage primaryStage) {
     primaryStage.show();
 }
 
-/**
- * Program entry point.
- * @param args Command line arguments.
- */
-public static void main(String[] args) {
-    printIntroMessage();
+/*------------------------------------------------
+ * PRIVATE METHODS
+ *----------------------------------------------*/
 
-    launch(args);
+/**
+ * Retrieves the program authors.
+ * @return An array of strings with the program authors' names.
+ */
+private static String[] getAuthors() {
+    return new String[] { "Philip Arvidsson (S133686)",
+                          "Martin Bergqvist (S141564)" };
 }
 
 /**
@@ -48,15 +73,6 @@ private static void printIntroMessage() {
         System.out.println("    " + author);
 
     System.out.println();
-}
-
-/**
- * Retrieves the program authors.
- * @return An array of strings with the program authors' names.
- */
-private static String[] getAuthors() {
-    return new String[] { "Philip Arvidsson (S133686)",
-                          "Martin Bergqvist (S141564)" };
 }
 
 }
