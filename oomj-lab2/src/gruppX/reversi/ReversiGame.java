c692b42b9e216b38e1e72dad5a0af92f38a78098
Philip Arvidsson <philiparvidsson@users.noreply.github.com>
2015-10-08 09:43:20 +0200

    Stuff.

--- oomj-lab2/src/gruppX/reversi/ReversiGame.java
+++ oomj-lab2/src/gruppX/reversi/ReversiGame.java
@@ -1,9 +1,22 @@
 package gruppX.reversi;
 
-public final class ReversiGame {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public final class ReversiGame extends Application {
+
+@Override
+public void start(Stage primaryStage) {
+    System.out.println("in start");
+
+    primaryStage.setTitle("hellooooo");
+    primaryStage.show();
+}
 
 public static void main(String[] args) {
     System.out.println("Nothing to see here!");
+
+    launch(args);
 }
 
 }
