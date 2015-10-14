e71e6903e11133ee3cd8bd95b07eaeba57436597
Philip Arvidsson <philiparvidsson@users.noreply.github.com>
2015-10-14 09:00:10 +0200

    recursionnnnnnnzzzzzzzz

--- oomj-lab2/src/grupp1/othello/model/GameGrid.java
+++ oomj-lab2/src/grupp1/othello/model/GameGrid.java
@@ -12,14 +12,14 @@ import grupp1.othello.exception.InvalidMoveException;
 
 /**
  * Grid setup for the game
- * 
+ *
  * @author Tkocz
  */
 public class GameGrid  {
 
 private final int gridSize;
 
-public int[][] gridCells;
+public int[][] grid;
 
 /*------------------------------------------------
  * PUBLIC METHODS
@@ -29,21 +29,21 @@ public GameGrid(int gridSize) {
     this.gridSize = gridSize;
 
     // Allocate a square game grid of the specified size.
-    gridCells = new int[gridSize][gridSize];
+    grid = new int[gridSize][gridSize];
 }
 /**
- * So far, checks whether the cell is empty, 
+ * So far, checks whether the cell is empty,
  * will expand to check for all valid moves this turn.
- * @param x 
+ * @param x
  * @param y
  * @param playerIndex current player
  * @return true for valid move, else false
  */
 public Boolean isValidMove(int x, int y, int playerIndex) {
-    if (gridCells[x][y] != 0)
+    if (grid[x][y] != 0)
         return (false);
 
-    return (true);
+    return (recurseDirections(x, y, playerIndex, false));
 }
 /**
  * Resets the grid, and sets the four middle cells to starting state
@@ -52,34 +52,42 @@ public void initialize(){
     int outer, inner;
     for(outer =0; outer<gridSize; outer++){
         for(inner =0; inner<gridSize; inner++){
-            gridCells[outer][inner] = 0;
+            grid[outer][inner] = 0;
         }
     }
-    gridCells[3][3] = 2;
-    gridCells[3][4] = 1;
-    gridCells[4][3] = 1;
-    gridCells[4][4] = 2;
-    printBoard();
+    grid[3][3] = 2;
+    grid[3][4] = 1;
+    grid[4][3] = 1;
+    grid[4][4] = 2;
+    printBoard(1);
 }
 
 public void placeMarker(int x, int y, int player) throws InvalidMoveException{
     if(!isValidMove(x,y,player)){
         throw(new InvalidMoveException("fasen"));
     }
-    gridCells[x][y] = player;
-    printBoard();
+    recurseDirections(x, y, player, true);
+    grid[x][y] = player;
+    if (player == 2)
+        printBoard(1);
+    else
+        printBoard(2);
     }
 /**
  * Måste ju skriva ut brädet på nåt sätt innan GUI
  */
-public void printBoard(){
+public void printBoard(int player){
         System.out.println(" _ _ _ _ _ _ _ _");
         for(int outer=0; outer<gridSize; outer++){
             for(int inner=0; inner<gridSize;inner++){
-                if(gridCells[inner][outer] == 0)
-                    System.out.print("|_");
+                if(grid[inner][outer] == 0) {
+                    if (!isValidMove(inner, outer, player))
+                        System.out.print("|_");
+                    else
+                        System.out.print("|x");
+                }
                 else
-                    System.out.print("|" +gridCells[inner][outer]);
+                    System.out.print("|" +grid[inner][outer]);
             }
             System.out.print("|\n");
 
@@ -88,4 +96,40 @@ public void printBoard(){
 /*------------------------------------------------
  * PRIVATE METHODS
  *----------------------------------------------*/
+
+private Boolean recurseDirections(int x, int y, int player, Boolean flip) {
+    return (recurseDirection(x, y, -1, -1, player, false, flip)
+         || recurseDirection(x, y, -1,  0, player, false, flip)
+         || recurseDirection(x, y, -1,  1, player, false, flip)
+         || recurseDirection(x, y,  0, -1, player, false, flip)
+         || recurseDirection(x, y,  0,  1, player, false, flip)
+         || recurseDirection(x, y,  1, -1, player, false, flip)
+         || recurseDirection(x, y,  1,  0, player, false, flip)
+         || recurseDirection(x, y,  1,  1, player, false, flip));
+}
+
+private Boolean recurseDirection(int x, int y, int dx, int dy, int player, Boolean ok, Boolean flip) {
+    x += dx;
+    y += dy;
+
+    // Out of bounds.
+    if (x < 0 || x >= 8 || y < 0 || y >= 8)
+        return (false);
+
+    // Empty grid.
+    if (grid[x][y] == 0)
+        return (false);
+
+    if (grid[x][y] == player)
+        return (ok);
+
+    ok = true;
+    Boolean result = recurseDirection(x, y, dx, dy, player, ok, flip);
+
+    if (result && flip)
+        grid[x][y] = player;
+
+    return (result);
+}
+
 }
