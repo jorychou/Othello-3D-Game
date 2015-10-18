4a0a4eda57ec059eb7f95ee6f38451d01de48c3d
Martin Bergqvist <s141564@student.hb.se>
2015-10-18 13:48:21 +0200

    added functionality (defect) for adding markers on mouseclick

--- oomj-lab2/src/grupp1/othello/view/GameBoard.java
+++ oomj-lab2/src/grupp1/othello/view/GameBoard.java
@@ -1,4 +1,7 @@
 package grupp1.othello.view;
+/*------------------------------------------------
+ * IMPORTS
+ *----------------------------------------------*/
 
 import grupp1.othello.controller.Player;
 import javafx.geometry.Insets;
@@ -11,12 +14,7 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.event.EventHandler;
-
-/*------------------------------------------------
- * IMPORTS
- *----------------------------------------------*/
-
-//import blah;
+import javafx.event.EventTarget;
 
 /*------------------------------------------------
  * CLASS
@@ -29,6 +27,7 @@ import javafx.event.EventHandler;
 public class GameBoard  {
     
 private GridPane board;
+
 /*------------------------------------------------
  * PUBLIC METHODS
  *----------------------------------------------*/
@@ -51,37 +50,50 @@ public GameBoard(){
                 StackPane tile = new StackPane();
                 tile.setMinSize(30,30);
                 tile.setStyle("-fx-border-color: white");
-                board.add(tile, i ,j);   
+                GridPane.setRowIndex(tile, i);
+                GridPane.setColumnIndex(tile, j);
+                //System.out.println( "Node: " + tile + " at " + GridPane.getRowIndex(tile) + "/" + GridPane.getColumnIndex(tile)); 
+                board.getChildren().add(tile);
             }
         }
         setPlacing(3,3, Color.WHITE);
         setPlacing(3,4, Color.BLACK);
         setPlacing(4,3, Color.BLACK);
         setPlacing(4,4, Color.WHITE);
-        board.setOnMouseClicked(e -> setPlacing(4,5, Color.WHITE));
         
-        /*
-        board.setOnMouseClicked((MouseEvent e) -> {
-            for(Node node: board.getChildren()){
-                if(node instanceof StackPane) {
-                    setPlacing(GridPane.getRowIndex(node), GridPane.getColumnIndex(node), Color.WHITE);
+        board.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent e) {
+                for(Node node : board.getChildren()){
+                    if(node instanceof StackPane){
+                        if(node.getBoundsInParent().contains(e.getSceneX(),  e.getSceneY())){
+                            
+                            System.out.println( "Node: " + node + " at x" + GridPane.getRowIndex( node) + "/ y" + GridPane.getColumnIndex( node)); 
+                            //Cannot for the life of me figure out why , but x0 is the row ABOVE first row, and the last row is out of bounds
+                            
+                            //the setPlacing works this way, except the last row is out of bounds, and exceptions are thrown to the left and right
+                            setPlacing(GridPane.getRowIndex( node)-1, GridPane.getColumnIndex( node), Color.BLACK);
+                            
+                            //@todo Check for validity against the GameGrid, and add if valid
+                            //probably want to replace the whole setPlacing for a replot-GameGrid-function
+                        }
+                    }
                 }
             }
         });
-        */
 }
 
 public GridPane getGameBoard(){
     return board;
 }
-
-private void setPlacing(int x, int y, Color color){
-    Circle marker = new Circle(10, color);
-    board.add(marker, x,y);
-    GridPane.setMargin(marker,new Insets(5));
-}
 /*------------------------------------------------
  * PRIVATE METHODS
  *----------------------------------------------*/
 
+private void setPlacing(int x, int y, Color color){
+    
+    Circle marker = new Circle(10, color);
+    board.add(marker, y, x);
+    GridPane.setMargin(marker,new Insets(5));
+}
 }
